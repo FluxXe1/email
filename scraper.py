c5ed1c46922464b88504679faf6eab49d06fe380
FluxXe <115143807+FluxXe1@users.noreply.github.com>
2022-10-09 06:41:55 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,9 +1,9 @@
-import collections import deque
+from collections import deque
 import re
 
 from bs4 import BeautifulSoup
 import requests
-import urlilib.parse
+import urllib.parse
 
 user_url =str(input('[+] Masukan url: '))
 urls = deque([user_url])
@@ -16,26 +16,26 @@ try:
         while True:
                count += 1
                if count > limit:
-                      break
+                         break
 
-                url = urls.popleft()
-                scraped_urls.add(url)
-                parts = urllib.parse.urlsplit(url)
-                base_url = f'{parts.scheme}://{part.netloc}'
-                path = url[:url.rfind('/')+1] if '/' in parts.path else url
+               url = urls.popleft()
+               scraped_urls.add(url)
+               parts = urllib.parse.urlsplit(url)
+               base_url = f'{parts.scheme}://{parts.netloc}'
+               path = url[:url.rfind ('/')+1] if '/' in parts.path else url
 
-                print(f'{count} Memproses {url}')
+               print(f'{count} Memproses {url}')
 
-                try:
+               try:
                        response = requests.get(url)
-                except(requests.exceptions.MissingScheme, requests.exceptions.ConnectionError):
+               except(requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
                        continue
 
-                new_emails = set(re.findall(r'[a-z0-9\.\-+_]+@\w+\.+[a-z\.]+', response.text, re.I))
-                emails.update(new_emails)
+               new_emails = set(re.findall(r'[a-z0-9\.\-+_]+@\w+\.+[a-z\.]+', response.text, re.I))
+               emails.update(new_emails)
 
-                soup = BeautifulSoup(response.text, 'html.parser')
-                for anchor in soup.find_all('a'):
+               soup = BeautifulSoup(response.text, 'html.parser')
+               for anchor in soup.find_all('a'):
                     link =anchor.attrs['href'] if 'href' in anchor.attrs else ''
                     if link.startswith('/'):
                         link = base_url + link
@@ -46,7 +46,7 @@ try:
                             urls.append(link)
 
 
-except keyboardInterrupt:
+except KeyboardInterrupt:
     print('[-] Closing!')
 
 print('\n Proses selesai!')
@@ -55,3 +55,4 @@ print(f'\n{len(emails)} email ditemukan \n ___________________')
 for mail in emails:
     print(' '+mail)
 print('\n ______________________')
+
